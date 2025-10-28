0c1676c4b4a0f413022aaac8ba9fa71b58c47d17
Simeon David Schaub <simeon@schaub.rocks>
2025-10-28 14:08:39 +0100

    implement wrapper

--- src/GFXInfo.jl
+++ src/GFXInfo.jl
@@ -1,5 +1,39 @@
 module GFXInfo
 
-# Write your package code here.
+using gfxinfo_c_bindings_jll
+
+export active_gpu
+
+mutable struct GPU
+    handle::Ptr{Cvoid}
+    GPU(handle::Ptr{Cvoid}) = finalizer(new(handle)) do gpu
+        @ccall libgfxinfo_c_bindings.gfxinfo_free_gpu(getfield(gpu, :handle)::Ptr{Cvoid})::Cvoid
+    end
+end
+
+function active_gpu()
+    handle = @ccall libgfxinfo_c_bindings.gfxinfo_active_gpu()::Ptr{Cvoid}
+    return GPU(handle)
+end
+
+function Base.getproperty(gpu::GPU, name::Symbol)
+    handle = getfield(gpu, :handle)
+    if name === :vendor
+        ptr = @ccall libgfxinfo_c_bindings.gfxinfo_get_vendor(handle::Ptr{Cvoid})::Cstring
+    elseif name === :model
+        ptr = @ccall libgfxinfo_c_bindings.gfxinfo_get_model(handle::Ptr{Cvoid})::Cstring
+    elseif name === :family
+        ptr = @ccall libgfxinfo_c_bindings.gfxinfo_get_family(handle::Ptr{Cvoid})::Cstring
+    elseif name === :device_id
+        ptr = @ccall libgfxinfo_c_bindings.gfxinfo_get_device_id(handle::Ptr{Cvoid})::Cstring
+    else
+        throw(FieldError(gpu, name))
+    end
+    str = unsafe_string(ptr)
+    @ccall libgfxinfo_c_bindings.gfxinfo_free_string(ptr::Cstring)::Cvoid
+    return str
+end
+
+Base.propertynames(::GPU) = (:vendor, :model, :family, :device_id)
 
 end
